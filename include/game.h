869f5109f573f0d420cf60c09783d66999ced6e8
aryanwalia24 <aryanwalia2303@gmail.com>
2024-09-02 17:01:50 +0530

    Game Flow + InputValidation

--- include/game.h
+++ include/game.h
@@ -6,24 +6,24 @@
 #include "poker_hand.h"
 
 // Constants
-#define MAX_PLAYERS 10
-#define MAX_NAME_LENGTH 50
 #define START_MONEY 1000
 #define SMALL_BLIND 10
 #define BIG_BLIND 20
 
 // Function prototypes
-void initializeGame(Player players[], int *numPlayers, Deck *deck);
-void startGame(Player players[], int numPlayers, Deck *deck);
-void playRound(Player players[], int numPlayers, Deck *deck);
+void initializeGame(Player players[], int *playerCount, Deck *deck);
+void startGame(Player players[], int *playerCount, Deck *deck);
+void playRound(Player players[], int playerCount, Deck *deck);
 int displayMenu();
 
-void setBlinds(Player players[], int numPlayers);
-void dealCards(Player players[], int numPlayers, Deck *deck);
-void executeBettingRound(Player players[], int numPlayers);
+void setBlinds(Player players[], int playerCount);
+void dealCards(Player players[], int playerCount, Deck *deck);
+void executeBettingRound(Player players[], int playerCount);
 void handleFlop(Deck *deck);
 void handleTurn(Deck *deck);
 void handleRiver(Deck *deck);
-void determineWinner(Player players[], int numPlayers);
+void determineWinner(Player players[], int playerCount);
+void displayPlayerCards(const Player *player, int display);
+void displayCommunityCards(const Card communityCards[], int numCards);
 
 #endif // GAME_H
--- src/game.c
+++ src/game.c
@@ -1,71 +1,82 @@
 #include "../include/game.h"
-#include <limits.h>
-#include <ctype.h>
+#include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
 
-static int getValidInt(int min, int max, const char *prompt);
+static int getValidIntGame(int min, int max, const char *prompt);
 static int getValidBet(int wallet, const char *prompt);
 
-void initializeGame(Player players[], int *numPlayers, Deck *deck)
+void initializeGame(Player players[], int *playerCount, Deck *deck)
 {
-    int playerCount = getValidInt(2, MAX_PLAYERS, "Enter the number of players (2-%d): ");
-    *numPlayers = playerCount;
+    printf("\nEnter the number of players (minimum 2): ");
+    *playerCount = 2;
 
-    char name[MAX_NAME_LENGTH];
-    for (int i = 0; i < playerCount; i++)
+    printf("\nEnter player names:\n");
+    for (int i = 0; i < *playerCount; i++)
     {
         printf("Enter name for player %d: ", i + 1);
-        scanf("%s", name);
-        if (createPlayer(i, name, START_MONEY, &players[i]) != 0)
+        if (fgets(players[i].name, MAX_NAME_LENGTH, stdin) != NULL)
         {
-            printf("Error creating player. Try again.\n");
-            i--;
+            size_t len = strlen(players[i].name);
+            if (len > 0 && players[i].name[len - 1] == '\n')
+            {
+                players[i].name[len - 1] = '\0';
+            }
+            createPlayer(i + 1, players[i].name, START_MONEY, &players[i]);
+        }
+        else
+        {
+            fprintf(stderr, "Error reading player name.\n");
+            exit(EXIT_FAILURE);
         }
     }
 
-    initializeDeck(deck);
+    initialiseDeck(deck);
     shuffleDeck(deck);
 }
 
-void startGame(Player players[], int numPlayers, Deck *deck)
+void startGame(Player players[], int *playerCount, Deck *deck)
 {
     int continueGame = 1;
+
     while (continueGame)
     {
         continueGame = displayMenu();
         if (continueGame)
         {
-            playRound(players, numPlayers, deck);
+            initializeGame(players, playerCount, deck);
+            playRound(players, *playerCount, deck);
         }
     }
-    printf("Game over. Thanks for playing!\n");
+    printf("\n---------- Game over. Thanks for playing Poker! ----------\n");
 }
 
 void playRound(Player players[], int numPlayers, Deck *deck)
 {
-    printf("\nStarting a new round...\n");
-    shuffleDeck(deck);
+    printf("\n--------------- Starting A New Round -------------\n");
+    printf("1. Setting Blinds \n\n");
 
-    printf("Setting blinds...\n");
+    printf("---------------------- Blinds ------------------------\n");
     setBlinds(players, numPlayers);
 
-    printf("Dealing cards to players...\n");
+    printf("\n-------------- Dealing Cards to Players ------------\n");
     dealCards(players, numPlayers, deck);
 
     executeBettingRound(players, numPlayers);
 
-    // Modes
+    printf("\n-------------- Revealing The FLOP -------------------\n");
     handleFlop(deck);
     executeBettingRound(players, numPlayers);
 
+    printf("\n-------------- Revealing The TURN -------------------\n");
     handleTurn(deck);
     executeBettingRound(players, numPlayers);
 
+    printf("\n-------------- Revealing The RIVER -------------------\n");
     handleRiver(deck);
     executeBettingRound(players, numPlayers);
 
     determineWinner(players, numPlayers);
-    printf("Round complete.\n");
 }
 
 int displayMenu()
@@ -73,19 +84,25 @@ int displayMenu()
     int choice;
     while (1)
     {
-        printf("\n--- Texas Hold'em Poker ---\n");
+        printf("\n------------------ Texas Hold'em Poker -------------\n\n");
         printf("1. Start a new round\n");
-        printf("2. Exit game\n");
+        printf("2. Exit game\n\n");
         printf("Choose an option: ");
 
-        choice = getValidInt(1, 2, "");
+        choice = getValidIntGame(1, 2, "");
 
         if (choice == 1)
-            return 1; // Continue game
+        {
+            return 1; // Start a new round
+        }
         else if (choice == 2)
+        {
             return 0; // Exit game
+        }
         else
+        {
             printf("Invalid choice. Please try again.\n");
+        }
     }
 }
 
@@ -96,66 +113,105 @@ void setBlinds(Player players[], int numPlayers)
         printf("Not enough players to set blinds.\n");
         return;
     }
+    printf("%s pays Small Blind: $%d\n", players[0].name, SMALL_BLIND);
+    printf("%s pays Big Blind: $%d\n", players[1].name, BIG_BLIND);
 
-    //  first player is the small blind, and the next is the big blind
-    int smallBlindIdx = 0;
-    int bigBlindIdx = 1;
-
-    updateWallet(&players[smallBlindIdx], -SMALL_BLIND);
-    updateWallet(&players[bigBlindIdx], -BIG_BLIND);
-
-    printf("%s pays the small blind of $%d\n", players[smallBlindIdx].name, SMALL_BLIND);
-    printf("%s pays the big blind of $%d\n", players[bigBlindIdx].name, BIG_BLIND);
+    players[0].wallet -= SMALL_BLIND; 
+    players[1].wallet -= BIG_BLIND;   
 }
 
 void dealCards(Player players[], int numPlayers, Deck *deck)
 {
+    if (numPlayers < 1)
+    {
+        printf("No players to deal cards to.\n");
+        return;
+    }
+
+    printf("Dealing cards...\n");
     for (int i = 0; i < numPlayers; i++)
     {
-        printf("%s receives two cards.\n", players[i].name);
-        drawCard(deck); // first card
-        drawCard(deck); // second card
+        players[i].hand[0] = drawCard(deck);
+        players[i].hand[1] = drawCard(deck);
     }
+}
+
+void displayPlayerCards(const Player *player, int display)
+{
+    if (display)
+    {
+        printf("%s's cards: ", player->name);
+        printCard(player->hand[0]);
+        printCard(player->hand[1]);
+        printf("\n");
+    }
+    else
+    {
+        printf("Player's cards are hidden.\n");
+    }
+}
+
+void displayCommunityCards(const Card communityCards[], int numCards)
+{
+    printf("Community cards: \n");
+    for (int i = 0; i < numCards; i++)
+    {
+        if (communityCards[i] != 0)
+        {
+            printCard(communityCards[i]);
+        }
+        else
+        {
+            printf("Invalid card ");
+        }
+    }
+    printf("\n");
 }
 
 void executeBettingRound(Player players[], int numPlayers)
 {
-    printf("Executing betting round...\n");
+    printf("\n------------ Betting Round ------------\n");
     for (int i = 0; i < numPlayers; i++)
     {
-        if (players[i].wallet > 0)
+        printf("\n%s's turn:\n", players[i].name);
+        int bet = getValidBet(players[i].wallet, "Enter bet amount (or 0 to check/fold): ");
+        if (bet > 0)
         {
-            int bet = getValidBet(players[i].wallet, "Enter bet amount (or 0 to check/fold): ");
-            if (bet > 0)
-            {
-                updateWallet(&players[i], -bet);
-                printf("%s bets $%d.\n", players[i].name, bet);
-            }
+            players[i].wallet -= bet;
+            printf("%s bets $%d.\n", players[i].name, bet);
+        }
+        else
+        {
+            printf("%s checks/folds.\n", players[i].name);
         }
     }
 }
 
-// flop (reveal 3 community cards)
 void handleFlop(Deck *deck)
 {
-    printf("Revealing the flop...\n");
-    drawCard(deck); // Card 1
-    drawCard(deck); // Card 2
-    drawCard(deck); // Card 3
+    printf("\nRevealing the flop...\n");
+    Card communityCards[3];
+    for (int i = 0; i < 3; i++)
+    {
+        communityCards[i] = drawCard(deck);
+    }
+    displayCommunityCards(communityCards, 3);
 }
 
-// turn (reveal 1 community card)
 void handleTurn(Deck *deck)
 {
-    printf("Revealing the turn...\n");
-    drawCard(deck);
+    printf("\nRevealing the turn...\n");
+    Card communityCards[1];
+    communityCards[0] = drawCard(deck);
+    displayCommunityCards(communityCards, 1);
 }
 
-// river (reveal 1 community card)
 void handleRiver(Deck *deck)
 {
-    printf("Revealing the river...\n");
-    drawCard(deck);
+    printf("\nRevealing the river...\n");
+    Card communityCards[1];
+    communityCards[0] = drawCard(deck);
+    displayCommunityCards(communityCards, 1);
 }
 
 void determineWinner(Player players[], int numPlayers)
@@ -167,7 +223,7 @@ void determineWinner(Player players[], int numPlayers)
     }
 
     Card communityCards[5];
-    PokerHand bestHands[MAX_PLAYERS];
+    PokerHand bestHands[2];
 
     for (int i = 0; i < numPlayers; i++)
     {
@@ -183,83 +239,45 @@ void determineWinner(Player players[], int numPlayers)
         }
     }
 
-    printf("The winner is %s with a %d!\n", players[winnerIndex].name, bestHands[winnerIndex].rank);
+    const char *handType = pokerHandToString(bestHands[winnerIndex].rank);
+    int amountWon = players[winnerIndex].wallet - START_MONEY;
+
+    printf("\n------ The Winner is %s with %s and has won $%d! ------\n", players[winnerIndex].name, handType, amountWon);
+
+    printf("\n-------------- Game Statistics -------------\n");
+    for (int i = 0; i < numPlayers; i++)
+    {
+        printf("%s's winnings: $%d\n", players[i].name, players[i].wallet - START_MONEY);
+        printf("%s's cards: ", players[i].name);
+        printCard(players[i].hand[0]);
+        printCard(players[i].hand[1]);
+        printf("\n");
+    }
+    printf("\nRound complete.\n");
 }
 
-static int getValidInt(int min, int max, const char *prompt)
+static int getValidIntGame(int min, int max, const char *prompt)
 {
-    char buffer[256];
     int value;
-
-    while (1)
+    do
     {
-        printf(prompt, max);
-        if (fgets(buffer, sizeof(buffer), stdin) != NULL)
-        {
-            char *ptr = buffer;
-            int isValid = 1;
-            while (*ptr != '\0' && *ptr != '\n')
-            {
-                if (!isdigit((unsigned char)*ptr))
-                {
-                    isValid = 0;
-                    break;
-                }
-                ptr++;
-            }
-
-            if (isValid)
-            {
-                value = strtol(buffer, NULL, 10);
-                if (value >= min && value <= max)
-                {
-                    return value;
-                }
-            }
-            printf("Invalid input. Please enter a number between %d and %d.\n", min, max);
-        }
-        else
-        {
-            printf("Error reading input. Please try again.\n");
-        }
-    }
+        printf(prompt);
+        scanf("%d", &value);
+        while (getchar() != '\n') // Clear input buffer
+            ;
+    } while (value < min || value > max);
+    return value;
 }
 
 static int getValidBet(int wallet, const char *prompt)
 {
-    char buffer[256];
     int bet;
-
-    while (1)
+    do
     {
         printf(prompt);
-        if (fgets(buffer, sizeof(buffer), stdin) != NULL)
-        {
-            char *ptr = buffer;
-            int isValid = 1;
-            while (*ptr != '\0' && *ptr != '\n')
-            {
-                if (!isdigit((unsigned char)*ptr))
-                {
-                    isValid = 0;
-                    break;
-                }
-                ptr++;
-            }
-
-            if (isValid)
-            {
-                bet = strtol(buffer, NULL, 10);
-                if (bet >= 0 && bet <= wallet)
-                {
-                    return bet;
-                }
-            }
-            printf("Invalid bet. You must bet between 0 and %d.\n", wallet);
-        }
-        else
-        {
-            printf("Error reading input. Please try again.\n");
-        }
-    }
+        scanf("%d", &bet);
+        while (getchar() != '\n')
+            ;
+    } while (bet < 0 || bet > wallet);
+    return bet;
 }
