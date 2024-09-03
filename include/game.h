f32509797fb05dbedddd3fbdc5e59bfeb832bbea
aryanwalia24 <aryanwalia2303@gmail.com>
2024-09-04 02:35:15 +0530

    Game.h Compatibility with other modules

--- include/game.h
+++ include/game.h
@@ -12,20 +12,20 @@
 #define MAX_PLAYERS 10
 #define INITIAL_POT 200
 
-// Function prototypes
-void initializeGame(Player players[], int *playerCount, Deck *deck);
-void startGame(Player players[], int *playerCount, Deck *deck);
-void playRound(Player players[], int playerCount, Deck *deck);
+void initializeGame(Player **players, int *playerCount, Deck *deck);
+void startGame(Player **players, int *playerCount, Deck *deck);
+void playRound(Player *players, int playerCount, Deck *deck);
 int displayMenu();
 
-void setBlinds(Player players[], int playerCount);
-void dealCards(Player players[], int playerCount, Deck *deck);
-void executeBettingRound(Player players[], int playerCount, const Card communityCards[], int *pot);
+void setBlinds(Player *players, int playerCount);
+void dealCards(Player *players, int playerCount, Deck *deck);
+void executeBettingRound(Player *players, int playerCount, const Card communityCards[], int *pot);
 void handleFlop(Deck *deck, Card communityCards[]);
 void handleTurn(Deck *deck, Card communityCards[]);
 void handleRiver(Deck *deck, Card communityCards[]);
-void determineWinner(Player players[], int playerCount, const Card communityCards[], int *pot);
+void determineWinner(Player *players, int playerCount, const Card communityCards[], int *pot);
 void displayPlayerCards(const Player *player, int display);
 void displayCommunityCards(const Card communityCards[], int numCards);
+void superCleanup(Player *players, int playerCount);
 
 #endif // GAME_H
