689e9663bb0fc51a47235d0685f8c8726df0375b
aryanwalia24 <aryanwalia2303@gmail.com>
2024-09-03 22:34:40 +0530

    Setup Pot Money & Players

--- include/game.h
+++ include/game.h
@@ -9,6 +9,8 @@
 #define START_MONEY 1000
 #define SMALL_BLIND 10
 #define BIG_BLIND 20
+#define MAX_PLAYERS 10
+#define INITIAL_POT 200
 
 // Function prototypes
 void initializeGame(Player players[], int *playerCount, Deck *deck);
@@ -18,11 +20,11 @@ int displayMenu();
 
 void setBlinds(Player players[], int playerCount);
 void dealCards(Player players[], int playerCount, Deck *deck);
-void executeBettingRound(Player players[], int playerCount);
-void handleFlop(Deck *deck);
-void handleTurn(Deck *deck);
-void handleRiver(Deck *deck);
-void determineWinner(Player players[], int playerCount);
+void executeBettingRound(Player players[], int playerCount, const Card communityCards[], int *pot);
+void handleFlop(Deck *deck, Card communityCards[]);
+void handleTurn(Deck *deck, Card communityCards[]);
+void handleRiver(Deck *deck, Card communityCards[]);
+void determineWinner(Player players[], int playerCount, const Card communityCards[], int *pot);
 void displayPlayerCards(const Player *player, int display);
 void displayCommunityCards(const Card communityCards[], int numCards);
 
